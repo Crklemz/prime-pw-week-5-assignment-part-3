49ffe14309a14e4ffa6375be42ae6c00c1f9ceae
Chris Klemz <crklemz@gmail.com>
2021-05-01 12:52:42 -0500

    basic function as required but does not work - troubleshooting

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,1 +1,17 @@
 console.log('***** Music Collection *****')
+
+
+
+const collection = [];
+
+function addToCollection (title, artist, yearPublished) {
+  let album = {
+    title: addToCollection.title,
+    artist: addToCollection.artist,
+    yearPublished: addToCollection.yearPublished,
+  }//End album/object creation
+  return album;
+  console.log(album);//Checking if album was created
+  collection.push (album); //Push new album/object to collection
+}//End addToCollection
+console.log(collection);//Checking if album was added to collection array
